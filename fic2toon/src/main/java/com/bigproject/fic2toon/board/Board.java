3ac946ebfc73702ac6830ddaaaee3a0c0ef80744
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-01-07 14:20:36 +0900

    초반수정

--- fic2toon/src/main/java/com/bigproject/fic2toon/board/Board.java
+++ fic2toon/src/main/java/com/bigproject/fic2toon/board/Board.java
@@ -6,6 +6,10 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Getter
 @Setter
 @Builder

26cc42cb96fa3ad1be504d11aa3764ca8b01ee03
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-01-17 15:49:49 +0900

    log저장 기능 개발진행중

--- fic2toon/src/main/java/com/bigproject/fic2toon/play/LogDto.java
+++ fic2toon/src/main/java/com/bigproject/fic2toon/play/LogDto.java
@@ -1,4 +1,15 @@
 package com.bigproject.fic2toon.play;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LogDto {
+    private String title;
+    private String path;
+    private String userUid;
+    private String createdTime;
 }
